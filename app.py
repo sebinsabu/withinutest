913a39e006fe1cdc8e8e42e06ca9a8701a374b89
Sebin Sabu <sebinsabu08@gmail.com>
2022-02-23 10:17:18 +0000

    template addition check

--- app.py
+++ app.py
@@ -4,5 +4,10 @@ from flask import Flaskapp = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route('/musicEmotion')
+def musicEmotion():
+    return render_template('musicEmotionView.html')
+
+
 if __name__ == "__main__":
    app.run(debug=True)
